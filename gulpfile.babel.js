f458be49f6fa05aa27c76be148fbbd106e922fea
jentleshin <jentleshin@gmail.com>
2020-09-15 05:17:00 +0900

    error on gh page

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
 import imagemin from "gulp-imagemin";
+import ghPages from "gulp-gh-pages";
 
 sass.compiler = require("node-sass");
 
@@ -39,12 +40,14 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
+const ghDeploy = () => gulp.src("dest/**/*").pipe(ghPages());
+
 const watch = () => {
   gulp.watch(routes.css.watch, styles);
   gulp.watch(routes.image.watch, optImg);
 };
 
-const clean = () => del(["dest/styles.css"]);
+const clean = () => del(["dest/styles.css", ".publish"]);
 
 const prepare = gulp.series([clean, optImg]);
 
@@ -53,3 +56,4 @@ const assets = gulp.series([styles]);
 const live = gulp.parallel([watch]);
 
 export const dev = gulp.series([prepare, assets, live]);
+export const deploy = gulp.series([prepare, assets, ghDeploy, clean]);
